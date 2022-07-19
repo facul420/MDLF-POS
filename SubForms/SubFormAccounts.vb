9751b7642b39ddadef6607829d5a9bfa28574375
John Angelo Midel <j.midel.488910@umindanao.edu.ph>
2022-07-19 23:39:12 +0800

    try

--- SubForms/SubFormAccounts.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class SubFormAccounts
-    Private Sub SubFormAccounts_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub Label5_Click(sender As Object, e As EventArgs)
-
-    End Sub
-End Class
